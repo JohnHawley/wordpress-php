35a2803f0ff261bdbc555ea9667b80b0c8bc0208
John Hawley <john@hawley.email>
2016-11-22 15:28:45 -0600

    Just playing around with the build.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,12 @@ if [ -n "$PHP_SENDMAIL_PATH" ]; then
      sed -i 's@^;sendmail_path.*@'"sendmail_path = ${PHP_SENDMAIL_PATH}"'@' /etc/php7/php.ini
 fi
 
+
+if [ -n "$PROD_SITEURL" ]; then
+     echo "define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . str_replace(DIRECTORY_SEPARATOR, '/', str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', dirname(__FILE__))));" >> /var/www/html/wp-config.php
+     echo "define('LIVE_SITEURL', '${PROD_SITEURL}');" >> /var/www/html/wp-config.php
+fi
+
 if [ "$PHP_XDEBUG_ENABLED" -eq "1" ]; then
      sed -i 's/^;zend_extension.*/zend_extension = xdebug.so/' /etc/php7/conf.d/00_xdebug.ini
 fi
