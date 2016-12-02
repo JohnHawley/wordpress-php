7690996b12d3850d661290035fc451c4a397b6c2
John Hawley <john@hawley.email>
2016-12-02 10:16:24 -0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,10 +7,10 @@ if [ -n "$PHP_SENDMAIL_PATH" ]; then
 fi
 
 
-if [ -n "$PROD_SITEURL" ]; then
-     echo "define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . str_replace(DIRECTORY_SEPARATOR, '/', str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', dirname(__FILE__))));" >> /var/www/html/wp-config.php
-     echo "define('LIVE_SITEURL', '${PROD_SITEURL}');" >> /var/www/html/wp-config.php
-fi
+#if [ -n "$PROD_SITEURL" ]; then
+#     echo "define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . str_replace(DIRECTORY_SEPARATOR, '/', str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', dirname(__FILE__))));" >> /var/www/html/wp-config.php
+#     echo "define('LIVE_SITEURL', '${PROD_SITEURL}');" >> /var/www/html/wp-config.php
+#fi
 
 if [ "$PHP_XDEBUG_ENABLED" -eq "1" ]; then
      sed -i 's/^;zend_extension.*/zend_extension = xdebug.so/' /etc/php7/conf.d/00_xdebug.ini
